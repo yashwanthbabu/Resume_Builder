37f5b9254716af69b66eb1d7d1119f554fb0b551
yashubabu <yashwanth.nas@gmail.com>
2014-05-27 15:39:30 +0530

    Update views.py

--- res1/views.py
+++ res1/views.py
@@ -12,6 +12,10 @@ import ho.pisa as pisa
 from django.template.loader import get_template,render_to_string
 
 from django.template import loader, Context
+from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
+from docx import Document
+
 
 def home(request):
     return render(request,'home.html',locals())
@@ -21,17 +25,19 @@ def registration(request):
         global email
 	fullname = request.POST.get("fullname")
 	email = request.POST.get("emailid")
+	if email and Name.objects.filter(Email_id=email):
+            return render(request,'email.html')
         #  Implement save function in mongodb
 	#import pdb;pdb.set_trace()
 	nam = Name.objects.create(Full_name=fullname,Email_id=email)
         print nam
         id = nam.id
         print id
-        return render(request,'education.html',{'objects_id':nam.id})
+        return render(request,'education.html',{'objects_id':nam.id,'mail':email,'name':fullname})
 	#nme = Name.Objects.all()
     else:
 	raise Http404
-    return render(request,'home.html',locals())
+    return render(request,'home.html',{'mail':email,'name':fullname})
 
    #return render(request,'education.html',{'objects':nam})
 def addmore(request):
@@ -50,13 +56,17 @@ def addmore(request):
         print name_obj
         edu.edn = name_obj
         edu.save()
-	
+	m_id = request.POST.get("m1")
+        print m_id
+	n_id = request.POST.get("n1")
+        print n_id
+        print request
 	cmp = request.POST.get("cmp",None)
 	if cmp == "true":
-	    return render(request,'professional.html',{'objects_id':obj_id})
+	    return render(request,'professional.html',{'objects_id':obj_id,'mail':m_id,'name':n_id,'qua':quafn,'univ':univ,'year':year,'perc':perc}})
     else:
 	raise Http404
-    return render(request,'education.html',{'objects_id':obj_id})
+    return render(request,'education.html',{'objects_id':obj_id,'mail':m_id,'name':n_id,'qua':quafn,'univ':univ,'year':year,'perc':perc}})
     
 
 
@@ -72,6 +82,19 @@ def profn(request):
 	pro = Profession.objects.create(Job_Type=job, Total_Experience_in_months=exp,Current_Industry=ind,Functional_Area=func,Key_Skills=key)
         obj_id = request.POST.get("y2")
         print obj_id
+        m_id = request.POST.get("m2")
+        print m_id
+        n_id = request.POST.get("n2")
+        print n_id
+        q_id = request.POST.get("q1")
+        print q_id
+        u_id = request.POST.get("u1")
+        print u_id
+        y_id = request.POST.get("y11")
+        print y_id
+        p_id = request.POST.get("p1")
+        print p_id
+        print request
         try:
             nam = Name.objects.get(id=obj_id)
             print nam
@@ -81,176 +104,136 @@ def profn(request):
         pro.save()
 	cmp = request.POST.get("cmp",None)
 	if cmp == "true":
-	    return render(request,'home.html',locals())
+	    return render(request,'contact.html',{'objects_id':obj_id,'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':job,'exp':exp,'ind':ind,'func':func,'key':key})
         else:
 	    raise Http404
-    return render(request,'professional.html',{'objects_id':obj_id})
-def email(request):
-    return render(request,'contact.html')
+    return render(request,'professional.html',{'objects_id':obj_id,'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':job,'exp':exp,'ind':ind,'func':func,'key':key})
+
 
 def send(request):
     
-    username = ""
+    username = "yyyy"
     print username
+    message = "hi"
     subject = "Confirmation mail"
     from_email = 'test@creyo.com'
+    a = Name.objects.all()
+    print a
+    b = Education.objects.all()
+    print b
+    c = Profession.objects.all()
+    print c
+    z_id = request.POST.get("z1")
+    print z_id
+    m_id = request.POST.get("m3")
+    print m_id
+    n_id = request.POST.get("n3")
+    print n_id
+    q_id = request.POST.get("q2")
+    print q_id
+    u_id = request.POST.get("u2")
+    print u_id
+    y_id = request.POST.get("y12")
+    print y_id
+    p_id = request.POST.get("p2")
+    print p_id
+    j_id = request.POST.get("j1")
+    print j_id
+    e_id = request.POST.get("e1")
+    print e_id
+    i_id = request.POST.get("i1")
+    print i_id
+    f_id = request.POST.get("f1")
+    print f_id
+    k_id = request.POST.get("k1")
+    print k_id
+    print request
+    to = [m_id]
+    #message = get_template('/home/naveen/Resume1/res1/templates/yashwanth1')
+    msg = get_template('/home/naveen/Resume1/res1/templates/c.txt').render(Context({'fullname':n_id,'mailing':m_id}))
+    radio = request.POST.get('imgControl')
+    print type(radio),'yashu',radio
+    if int(radio) == 0:
+        print 'radio'
+        message = get_template('/home/naveen/Resume1/res1/templates/Format1.html').render(Context({'fullname':n_id,'mailing':m_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id}))
+        #result = StringIO.StringIO()
+        result = StringIO.StringIO()
+        result.writelines(message)
+        #pdf = pisa.pisaDocument(StringIO.StringIO(message), result)
+        try:
+        
+            mail = EmailMessage(subject,msg,from_email,to)
+            #attachment = myview(request)
+            mail.attach('Resume.docx',result.getvalue(),'application/ms-word')
+            mail.send(msg)
+            return render(request,'text.html',{'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return render(request,'text.html',{'objects_id':z_id,'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
     
-    to = ['yashwanthbabu.gujarathi@gmail.com']
-    message = render_to_string('/home/naveen/Resume1/res1/templates/c.txt')
-    """username = request.POST.get('username', '')
-    from_email = request.POST.get('from_email', '')"""
-    """message = request.POST.get('message', '')
-    print username
-    print from_email
-    print message
-    result = pdf(request)
-    file_to_be_sent = pdf(request)
-    if username and from_email and message:"""
-    try:
-        send_mail(subject,message,from_email,['yashwanthbabu.gujarathi@gmail.com'])    
-    except BadHeaderError:
-        return HttpResponse('Invalid header found.')
-    return HttpResponse('sent successfully')
-    """else:
-        # In reality we'd use a form class
-        # to get proper validation errors.
-        return HttpResponse('Make sure all fields are entered and valid.')"""
+    elif int(radio) == 1:
+        print 'radio'
+        message = get_template('/home/naveen/Resume1/res1/templates/Format2.html').render(Context({'fullname':n_id,'mailing':m_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id}))
+        #result = StringIO.StringIO()
+        result = StringIO.StringIO()
+        result.writelines(message)
+        #pdf = pisa.pisaDocument(StringIO.StringIO(message), result)
+        try:
+        
+            mail = EmailMessage(subject,msg,from_email,to)
+            #attachment = myview(request)
+            mail.attach('Resume.docx',result.getvalue(),'application/ms-word')
+            mail.send(msg)
+            return render(request,'text.html',{'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return render(request,'text.html',{'objects_id':z_id,'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
 
-"""
-def mail(request):
-    if request.is_ajax() and request.method =="POST":
-        Full_name = request.POST.get('fullname', '')
-        Mail_id = request.POST.get('emailid', '')
-        print Full_name
-        print Mail_id
-        if Full_name and Mail_id:
-            try:
-                send_mail(fullname, emailid, ['to@example.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-    return HttpResponseRedirect('/home/')
-"""
-"""
-app = Flask(__name__)
-mail_ext = Mail(app)
-@app.route('/your/url')
-def pdfview():
-    subject = "Mail with PDF"
-    receiver = "receiver@mail.com"
-    mail_to_be_sent = Message(subject=subject, recipients=[receiver])
-    mail_to_be_sent.body = "This email contains PDF."
-    pdf = create_pdf(render_template('your/template.html'))
-    mail_to_be_sent.attach("file.pdf", "application/pdf", pdf.getvalue())
-    mail_ext.send(mail_to_be_sent)
-    return redirect(url_for('other_view'))
-
-"""
-"""
-def convert(data, home.html.pdf, open=False):
-    pdfFile = file(filename, "wb")
-    pdf = pisa.CreatePDF(
-       cStringIO.StringIO(data.encode("ISO-8859-1")),pdfFile)
-    pdfFile.close()
-    if not pdf.err:
-        pisa.startViewer(filename)
-"""
-# Create your views here.
-"""
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
-from res1.models import *
-from django.shortcuts import render_to_response
-
-def home(request):
-    if request.method =='POST':
-        fname = request.GET.get("fullname")
-        eid = request.GET.get("emailid")
-    try:
-        Name.objects.get(Full_name=fname,Email_id=eid)
-  	return render_to_response("error_registration.html",{}, RequestContext(request))
-    except:
-	Name.objects.create(Full_name=fname,Email_id=eid)
-    return render(request,'page1.html')
-def edn(request):
-    if request.method=='POST':
-        qualfn = request.POST.get("quafn")
-        univ = request.POST.get("univ")
-        year = request.POST.get("year")
-        perc = request.POST.get("perc")
-        edn = Education.objects.create(Qualification=qualfn,University=univ,Year_of_Passing=year,Aggrigation=perc)
-    return render_to_response('page2.html')
-def profn(request):
-    job = request.POST.get("job")
-    exp = request.POST.get("exp")
-    ind = request.POST.get("ind")
-    fun = request.POST.get("fun")
-    key = request.POST.get("key")
-    Profession.objects.create(Job_Type=job,Total_Experience_in_months=exp,Current_Industry=ind,Functional_Area=fun, Key_Skills=key)
-   return render(request, 'page3.html')
-
-from django.shortcuts import render
-from django.template.context import RequestContext
-from django.shortcuts import render_to_response
-from res1.models import Name,Education,Profession
-
-def page1(request):
-    fullname=request.GET.get("fullname")
-    emailid=request.GET.get("emailid")
-    d=Name()
-    d.Full_name=fullname
-    d.Email_id=emailid
-    d.save()
-    d=Name.objects.all()
-    return render(request, 'page1.html')
-
-def page2(request):
-    qualification = request.POST.get("quafn")
-    university = request.POST.get("univ")
-    year = request.POST.get("year")
-    aggrigation = request.POST.get("perc")
-    try:
-	Education.objects.get(Qualification=qualification,University=university,Year_of_Passing=year,Aggrigation=aggrigation)
-	return render_to_response("error_registration.html",{}, RequestContext(request))
-    except:
-	Education.objects.create(Qualification=qualification,University=university,Year_of_Passing=year,Aggrigation=aggrigation)
-	return render_to_response('page2.html')
-
-def page3(request):
-    jobtype = request.POST.get("job")
-    experience = request.POST.get("exp")
-    industry = request.POST.get("ind")
-    functarea = request.POST.get("func")
-    keyskills = request.POST.get("key")
-    try:
-	Qualification.objects.get(Job_Type=jobtype,Total_Experience_in_months=experience,Current_Industry=industry,Functional_Area=functarea,Key_Skills=keyskills)
-	return render_to_response("error_registration.html",{}, RequestContext(request))
-    except:
-	Qualification.objects.create(Qualification=qualification,University=university,Year_of_Passing=year,Aggrigation=aggrigation)
-	return render_to_response('page3.html', {"tag":True}, RequestContext(request))
-
-
-	fullname=request.GET.get("fullname")
-	emailid=request.GET.get("emailid")
-	
-	d=Data()
-    	d.fullname=fullname
-	d.emailid=emailid
-   
-	d.save()
-	return HttpResponseRedirect('/edn')
-	d=Data.objects.all()
-       return render_to_response(request, 'page1.html')
-
-    if request.method == "POST":
-	fullname = request.POST.get("fullname")
-	emailid = request.POST.get("emailid")
-	Data.objects.create(fullname=fullname,emailid=emailid)
-    	return render_to_response("page1.html",{}, RequestContext(request))
+    elif int(radio) == 2:
+        print 'radio'
+        message = get_template('/home/naveen/Resume1/res1/templates/Format3.html').render(Context({'fullname':n_id,'mailing':m_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id}))
+        #result = StringIO.StringIO()
+        result = StringIO.StringIO()
+        result.writelines(message)
+        #pdf = pisa.pisaDocument(StringIO.StringIO(message), result)
+        try:
+        
+            mail = EmailMessage(subject,msg,from_email,to)
+            #attachment = myview(request)
+            mail.attach('Resume.docx',result.getvalue(),'application/ms-word')
+            mail.send(msg)
+            return render(request,'text.html',{'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return render(request,'text.html',{'objects_id':z_id,'mail':m_id,'name':n_id,'qua':q_id,'univ':u_id,'year':y_id,'perc':p_id,'job':j_id,'exp':e_id,'ind':i_id,'func':f_id,'key':k_id})
+    
     else:
-	return render_to_response("page2.html",{},RequestContext(request))
-def page2(request):
-	return render(request, 'page2.html')
-def page3(request):
-	return render(request, 'page3.html')
+        return HttpResponse('hai')
+    
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = StringIO.StringIO()
 
-"""
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), mimetype='application/pdf')
+    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+def myview(request):
+    
+    #Retrieve data or whatever you need
+    return render_to_pdf(
+	    #'127.0.0.1:8000/',
+            'sample.html',
+	    #'education.html',
+	    #'professional.html',
+            {
+		'Full_name':'fullname',
+		'Email_id':'emailid',
+	            }
+        )
+
+
+
